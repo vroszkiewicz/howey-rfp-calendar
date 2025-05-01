382b1c8718c1cea1563596307ce89a79507d8959
vroszkiewicz <VRoszkiewicz@howey.org>
2025-05-01 09:50:28 -0400

    Update rfp_schedule_app.py

--- rfp_schedule_app.py
+++ rfp_schedule_app.py
@@ -102,7 +102,7 @@ if rfp_posted_date:
         {
             "Event": event,
             "Date": date.strftime('%B %d, %Y') if isinstance (date, datetime) else date,
-            "Days Left": (date.date() - today).days if isinstance(date, datetime) else ""
+            "Days Left": (date - today).days if isinstance(date, (datetime, date)) else ""
         }
         for event, date in schedule.items()
     ])
